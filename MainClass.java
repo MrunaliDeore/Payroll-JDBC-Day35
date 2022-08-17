f2ffa6adca759922cd2980db1463e216b39ec2f5
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-17 21:59:22 +0530

    Added : message in that class file

---
+++ MainClass.java
@@ -0,0 +1,8 @@
+package JDBC;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("Assignment no. 32\n");
+		System.out.println("Employee Payroll JDBC remaining Uc's  7 to 12");
+	}
+}
